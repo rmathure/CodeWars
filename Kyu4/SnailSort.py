11f77b2c5ec660607b74f9177b887fdc92a02206
rmathure <rmathure@indiana.edu>
2015-07-19 17:09:31 -0500

    Snail sort algorithm working... booyah

--- Kyu4/SnailSort.py
+++ Kyu4/SnailSort.py
@@ -26,8 +26,28 @@ NOTE: The idea is not sort the elements from the lowest value to the highest; th
 NOTE 2: The 0x0 (empty matrix) is represented as [[]]
 '''
 
-def snail(snailArray):
-    return
+
+
+
+def snail(snailArray,retArray=list()):
+    if len(snailArray)==0:
+        return retArray
+    for i in range(len(snailArray[0])-1):#traverse horizontal
+        retArray.append(snailArray[0][i])
+        snailArray[0][i]=None
+    for i in range(len(snailArray)-1): #traverse last column
+        retArray.append(snailArray[i][len(snailArray[i])-1])
+        snailArray[i][len(snailArray[i])-1]=None
+    for i in range(len(snailArray[len(snailArray)-1]))[::-1]: #Traverse last row in reverse
+        retArray.append(snailArray[len(snailArray)-1][i])
+        snailArray[len(snailArray)-1][i]=None
+    for i in range(1,len(snailArray)-1)[::-1]:#traverse first column in reverse
+        retArray.append(snailArray[i][0])
+        snailArray[i][0]=None
+    for i in range(len(snailArray)):
+        snailArray[i]=[x for x in snailArray[i] if x is not None]
+    snailArray=[x for x in snailArray if len(x)!=0]
+    return snail(snailArray,retArray)
 
 
 class TestSnailSort(unittest.TestCase):
@@ -36,12 +56,12 @@ class TestSnailSort(unittest.TestCase):
          [4,5,6],
          [7,8,9]]
         expected = [1,2,3,6,9,8,7,4,5]
-        self.assertEqual(snail(array), expected)
+        self.assertEqual(snail(array,[]), expected)
 
 
         array = [[1,2,3],
          [8,9,4],
          [7,6,5]]
         expected = [1,2,3,4,5,6,7,8,9]
-        self.assertEqual(snail(array), expected)
+        self.assertEqual(snail(array,[]), expected)
 
